2defa990a76ef0be44e84b5b3f97e0da304fc079
Efrem <BarefootEfrem@gmail.com>
2015-09-29 14:39:18 -0700

    first round of room-pricing analysis

--- invoice.py
+++ invoice.py
@@ -423,19 +423,35 @@ else:
 
 
 ##   *********** ANALYSIS *******************
-df_invoices = df.set_index('invoice')
-tot_fields = ['DATE','SUBTOTAL','DISCOUNT','TOTAL','membership','discount_type','day_type','duration']
-df_tots = df_invoices.query('item_type == "total"')[tot_fields]
+columns_to_fill = ['membership','discount_type']
+df[columns_to_fill] = df[columns_to_fill].fillna('??')
 
-# df_tots.mean() is mean income w and w/o discount
-
-# The following is a subset of df consisting of only invoices containing a given query
-def df_query(query):
-    return df_invoices.loc[df_invoices.query(query).index].reset_index()
-
-# This gives the max num of HOURS of any item in each invoice
-# df.groupby('invoice')['HOURS_UNITS'].max()
+df_invoices = df.set_index(['invoice','DATE'])
 
 ##  Line items for only rooms, not including other associated invoice items
-df_rooms = df_invoices.query('item_type == "room"').drop(['item_type'],axis=1).sort('item')
+df_rooms_only = df.query('item_type == "room"').drop(['item_type'], axis=1)
+df_rooms_only['day'] = (df_rooms_only['HOURS_UNITS'] < 5.5).map({True:'Half', False:'Full'})
+
+grouped_by_room = df_rooms_only.groupby(['item','day','membership','discount_type'])
+
+grouped_by_room['HOURS_UNITS','SUBTOTAL','TOTAL'].sum().to_excel('rooms_only_sum.xlsx')
+grouped_by_room['AMOUNT','HOURS_UNITS','SUBTOTAL','DISCOUNT','TOTAL'].mean().to_excel('rooms_only_avg.xlsx')
+
+
+"""
+# The overall length of time for each (invoice,DATE)
+event_hours = df.groupby(['invoice','DATE'])['HOURS_UNITS'].max()
+
+tot_fields = ['HOURS_UNITS','SUBTOTAL','DISCOUNT','TOTAL','membership','discount_type','day_type','duration']
+df_tots = df_invoices.query('item_type == "total"')[tot_fields]
+# df_tots2 = df.query('item_type == "total"').groupby(['invoice','DATE'])[tot_fields]
+
+# df_tots['HOURS_UNITS'] = event_hours
+
+# print df_tots
+# df_tots = df_tots['HOURS_UNITS'].update(event_hours)
+# print df_tots
+
+# df_tots.mean() is mean income w and w/o discount
+"""
 
