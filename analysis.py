8cc57e818e5df7968cacac3b3fe2e02822675f45
Efrem <BarefootEfrem@gmail.com>
2015-10-30 23:04:35 -0700

    basic analysis

--- analysis.py
+++ analysis.py
@@ -9,24 +9,39 @@ Created on Mon Aug 17 14:23:20 2015
 import pandas as pd
 NaN = pd.np.nan
 
-# Read 'clean' data into a dataframe
-df = pd.read_excel('invoice_data.xlsx', encoding='utf-8')
+# Read data into a dataframe
+df = pd.read_csv('invoice_data.csv')
+
+
+categorical_data = [
+ 'item_type','item','membership','discount_type','day_type','day_dur'
+]
+
+# for col in categorical_data:
+#     df[col] = df[col].astype('category')
+
+
+
 
 ##   *********** ANALYSIS *******************
-columns_to_fill = ['membership','discount_type']
-df[columns_to_fill] = df[columns_to_fill].fillna('??')
-
 df_invoices = df.set_index(['invoice','DATE'])
 
 ##  Line items for only rooms, not including other associated invoice items
-df_rooms_only = df.query('item_type == "room"').drop(['item_type'], axis=1)
+df_rooms_only = df.query('item_type == "ROOM"').drop(['item_type'], axis=1)
 df_rooms_only['day'] = (df_rooms_only['HOURS_UNITS'] < 5.5).map({True:'Half', False:'Full'})
 
 grouped_by_room = df_rooms_only.groupby(['item','day','membership','discount_type'])
 
 ## Output Room rental summaries as Excel sheets
-# grouped_by_room['HOURS_UNITS','SUBTOTAL','TOTAL'].sum().to_excel('rooms_only_sum.xlsx')
-# grouped_by_room['AMOUNT','HOURS_UNITS','SUBTOTAL','DISCOUNT','TOTAL'].mean().to_excel('rooms_only_avg.xlsx')
+grouped_by_room['HOURS_UNITS','SUBTOTAL','TOTAL'].sum().to_csv('rooms_only_sum.csv')
+grouped_by_room['AMOUNT','HOURS_UNITS','SUBTOTAL','DISCOUNT','TOTAL'].mean().to_csv('rooms_only_avg.csv')
+
+
+table = pd.pivot_table(df_rooms_only,
+                        index=['item','day','membership','discount_type'],
+                        values=['HOURS_UNITS','SUBTOTAL','TOTAL'],
+                        aggfunc=[pd.np.sum, pd.np.mean], fill_value=0 ) 
+table.to_csv('pivot.csv')
 
 
 """
